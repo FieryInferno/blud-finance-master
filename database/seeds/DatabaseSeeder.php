56b6b3750569aca01cf52356adcae8fc8166df9d
FieryInferno <bagassetia271@gmail.com>
2020-12-21 18:53:23 +0700

    Pembuatan seeder prefix penomoran

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             PrefixPenomoranTableSeeder::class,
-            RekeningBendaharaTableSeeder::class,
-            StsNLSeeder::class,
+            // RekeningBendaharaTableSeeder::class,
+            // StsNLSeeder::class,
         ]);
     }
 }
